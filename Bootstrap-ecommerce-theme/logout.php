375900313837f74d32ddb1a0af8a4e4258250dd2
gorkemguzeler <gorkemguzeler@sabanciuniv.edu>
2021-01-08 19:38:58 +0300

    update-g

--- Bootstrap-ecommerce-theme/logout.php
+++ Bootstrap-ecommerce-theme/logout.php
@@ -1,6 +1,21 @@
 <?php
+
+include "config.php";
+
 session_start();
+
+$a = $_SESSION['id'];
+
+$sql_command = "DELETE FROM BASKET WHERE customer_id = $a ";
+
+$myresult = mysqli_query($db, $sql_command);
+
+mysqli_fetch_assoc($myresult);
+
 session_destroy();
+
+
+
 // Redirect to the login page:
 header('Location: index.php');
 ?>
