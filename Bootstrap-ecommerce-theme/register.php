a83c6d40b3870a89e152217a8bef2eb79c63e178
Begum Altunbas <bgm.altunbas@gmail.com>
2021-01-09 00:41:22 +0300

    beg

--- Bootstrap-ecommerce-theme/register.php
+++ Bootstrap-ecommerce-theme/register.php
@@ -1,5 +1,6 @@
 <?php
 // Change this to your connection info.
+//session_start();
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
@@ -35,25 +36,37 @@ if ($stmt = $con->prepare('SELECT user_id, password FROM USERS WHERE username = 
 		echo 'Username exists, please choose another!';
     } 
     else {
-        // Username doesnt exists, insert new account
-        if ($stmt = $con->prepare('INSERT INTO USERS (username, password, first_name,last_name) VALUES (?, ?, ?,?)')) {
-	    // We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
-	    //$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-	    $stmt->bind_param('ssss', $_POST['username'], $_POST['password'], $_POST['first_name'],$_POST['last_name']);
-	    $stmt->execute();
+
+		include "config.php";
+		// Username doesnt exists, insert new account
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$first_name = $_POST['first_name'];
+		$last_name = $_POST['last_name'];
+		$phone = $_POST['phone'];
+		$address = $_POST['address'];
+		$email = $_POST['email'];
+
+
+		$sql_statement = "INSERT INTO USERS (username, password,first_name ,last_name)VALUES ('$username','$password', '$first_name','$last_name')";
+		mysqli_query($con, $sql_statement);
+
+		//$q = "SELECT user_id FROM USERS BY user_id DESC LIMIT 1" ;
+		$q = "SELECT MAX(user_id) AS C  FROM USERS WHERE user_id>0";
+		$result = mysqli_query($db, $q);
+
+		$row = mysqli_fetch_assoc($result)["C"];
+
+
+		$sql_statement2= "INSERT INTO CUSTOMER (customer_id, phone, address,email) VALUES ( '$row', '$phone', '$address','$email')";
+		mysqli_query($db, $sql_statement2);
+
         echo 'You have successfully registered, you can now login!';
         header('Location: login.html');
     }
-         else {
-             // Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
-             echo 'Could not prepare statement!';
-            }
+        
         // Insert new account
 	}
 	$stmt->close();
-} else {
-	// Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
-	echo 'Could not prepare statement!';
-}
 $con->close();
 ?>
