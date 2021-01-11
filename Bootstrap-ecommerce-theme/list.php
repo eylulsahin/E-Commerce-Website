ddab4eec4ebb747280006b6aa277ef7c0819786c
Eylul Sahin <seylul@sabanciuniv.edu>
2021-01-11 23:00:25 +0300

    update eyl

--- Bootstrap-ecommerce-theme/list.php
+++ Bootstrap-ecommerce-theme/list.php
@@ -68,7 +68,7 @@ session_start();
 
 
 
-<title>Filter Tutorial on Technopoints</title>
+<title>Coffee House</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -380,7 +380,7 @@ session_start();
  	$query = $_GET["searchfor"];
     $sql_statement = "SELECT *
                     FROM PRODUCT P, CATEGORY C
-                    WHERE (P.name LIKE '%$query%') OR (P.model LIKE '%$query%') OR ( C.category_name LIKE '%$query%') AND P.category_id = C.category_id
+                    WHERE ((P.name LIKE '%$query%') OR (P.model LIKE '%$query%') OR ( C.category_name LIKE '%$query%')) AND P.category_id = C.category_id
                   GROUP BY P.product_id";
     $result = mysqli_query($db,$sql_statement);
  }
